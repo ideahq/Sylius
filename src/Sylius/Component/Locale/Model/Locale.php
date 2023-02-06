ebe123e68a4052ac87a3d73716fc7b2c92bbeba6
jman <jose@petdirect.co.nz>
2023-02-07 11:15:54 +1300

    Use Locales::getName

--- src/Sylius/Component/Locale/Model/Locale.php
+++ src/Sylius/Component/Locale/Model/Locale.php
@@ -15,6 +15,7 @@ namespace Sylius\Component\Locale\Model;
 
 use Sylius\Component\Resource\Model\TimestampableTrait;
 use Symfony\Component\Intl\Intl;
+use Symfony\Component\Intl\Locales;
 
 class Locale implements LocaleInterface
 {
@@ -65,6 +66,6 @@ class Locale implements LocaleInterface
      */
     public function getName(?string $locale = null): ?string
     {
-        return Intl::getLocaleBundle()->getLocaleName($this->getCode(), $locale);
+        return Locales::getName($this->getCode(), $locale);
     }
 }
